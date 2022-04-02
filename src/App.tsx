507b421ebc836aac33ab6c67f90695e3d98bce4f
Thomas M <tjm165@case.edu>
2022-04-02 13:51:35 -0500

    checkout app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { TfGenerator } from "./components";
+import "./App.css";
+import { Header } from "./components/static";
 
 function App() {
+  const [shouldRefetch, setShouldRefetch] = useState("default");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header shouldRefetch={shouldRefetch} />
+      <TfGenerator setShouldRefetch={setShouldRefetch} />
     </div>
   );
 }
